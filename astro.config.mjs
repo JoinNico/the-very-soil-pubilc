2ebdfd134fde2455cfa35c3382f9bd47850f6d17
JoinNico <149471066+JoinNico@users.noreply.github.com>
2026-08-25 16:26:55 +0800

    Init commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
+import tailwindcss from "@tailwindcss/vite";
+import partytown from '@astrojs/partytown';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [sitemap(), partytown()],
+	output: 'server',
+	adapter: vercel(),
+	vite: {
+		plugins: [tailwindcss()],
+	},
+});
